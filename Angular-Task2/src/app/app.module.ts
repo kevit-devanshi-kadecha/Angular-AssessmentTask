2ba7c36b644c70498ce1988a789d0071fe941542
kevit-devanshi-kadecha <devanshi.kadecha@kevit.io>
2025-03-28 13:15:00 +0530

    Included login component and implemented routing for it

--- Angular-Task2/src/app/app.module.ts
+++ Angular-Task2/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
